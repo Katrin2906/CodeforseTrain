e794ffb3517f29348482ce89efa6de3577d81f32
Katrin <Karkusha869@gmail.com>
2022-10-01 01:19:56 +0300

    done

--- src/main/java/Words.java
+++ src/main/java/Words.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Words {
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>(4);
+        list.add(0, "word");
+        list.add(1, "localization");
+        list.add(2, "internationalization");
+        list.add(3, "pneumonoultramicroscopicsilicovolcanoconiosis");
 
+        for (String element : list) {
+            if (element.length() > 10) {
+                StringBuilder newElement = new StringBuilder(element);
+                newElement.replace(1, element.length()-1, String.valueOf((element.length()-2)));
+                System.out.println(newElement);
+            } else {
+                System.out.println(element);
+            }
+        }
     }
 }
