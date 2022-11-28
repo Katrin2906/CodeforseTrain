3b3cb9819d5927a74435ea23d2a82a81af371348
Katrin <Karkusha869@gmail.com>
2022-11-29 00:55:13 +0200

    everyday commit

---
+++ src/main/java/LeetCodeArrays/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,17 @@
+package LeetCodeArrays;
+
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int counter = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            // If the current element is equal to the next element, we skip
+            if (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+                continue;
+            }
+            // We will update the array in place
+            nums[counter] = nums[i];
+            counter++;
+        }
+        return counter;
+    }
+}
